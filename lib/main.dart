aac43578e4e1900630efa1552d14ca2a91f20223
Dave Faliskie <davefaliskie@gmail.com>
2024-01-05 21:02:18 -0500

    Flutter image example

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,8 +37,57 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Image Example"),
       ),
-      body: const Column(
-        children: [],
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 30.0),
+            ClipOval(
+              child: Image.asset(
+                "assets/images/Roads.png",
+                height: 100,
+              ),
+            ),
+            const Text("Asset Image"),
+            const SizedBox(height: 30.0),
+
+            // Network Image
+            SizedBox(
+              height: 200,
+              child: Image.network(
+                "https://roadsaudioassets.s3.amazonaws.com/RoadsShareLink.jpg",
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return const Icon(
+                    Icons.error,
+                    size: 50,
+                    color: Colors.red,
+                  );
+                },
+              ),
+            ),
+            const Text("Network Image"),
+
+            const SizedBox(height: 30.0),
+
+            SizedBox(
+              height: 200,
+              child: CachedNetworkImage(
+                imageUrl:
+                    "https://roadsaudioassets.s3.amazonaws.com/RoadsShareLink.jpg",
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            ),
+            const Text("Cached Network Image"),
+          ],
+        ),
       ),
     );
   }
